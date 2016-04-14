8eac1c8a52980d9c221b5e73e00db304b343edbc
lixinchn <lixinit@gmail.com>
2016-04-14 13:45:15 +0800

    remove element

---
+++ src/0027_RemoveElement.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+
+        new_nums_point = -1
+        for i in range(len(nums)):
+            if nums[i] == val:
+                continue
+            new_nums_point += 1
+            nums[new_nums_point] = nums[i]
+        print nums
+        return new_nums_point + 1
+        
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [3]
+    print solution.removeElement(nums, 3)

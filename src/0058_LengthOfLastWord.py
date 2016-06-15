f8904c967308c5d9b8d0cbf658530f9dac23b551
lixinchn <lixinit@gmail.com>
2016-06-15 10:21:00 +0800

    length of last word

---
+++ src/0058_LengthOfLastWord.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.rstrip().split(' ')[-1])
+        
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.lengthOfLastWord('a ')

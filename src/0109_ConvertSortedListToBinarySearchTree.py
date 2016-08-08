68cd406f390f3265d57b4c97760efc0df65366e3
lixinchn <lixinit@gmail.com>
2016-08-08 17:20:21 +0800

    convert sorted list to binary search tree

---
+++ src/0109_ConvertSortedListToBinarySearchTree.py
@@ -0,0 +1,90 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    @staticmethod
+    def create(arr):
+        head = None
+        this = None
+        left, right = False, False
+        prev_nodes = []
+        for val in arr:
+            node = TreeNode(val)
+            if not head:
+                head = node
+                this = node
+                continue
+
+            if left and right:
+                this = prev_nodes.pop(0)
+                left = False
+                right = False
+            if not left:
+                this.left = node
+                left = True
+            elif not right:
+                this.right = node
+                right = True
+            if node and node.val:
+                prev_nodes.append(node)
+
+        return head
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    @staticmethod
+    def create(arr):
+        head = None
+        this = None
+        for val in arr:
+            node = ListNode(val)
+            if not head:
+                head = node
+                this = node
+                continue
+
+            this.next = node
+            this = node
+        return head
+
+    def _print(self):
+        temp = self
+        result = ''
+        while temp:
+            result += str(temp.val) + ','
+            temp = temp.next
+        print result
+
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+        return self.do(nums, 0, len(nums) - 1)
+
+    def do(self, nums, begin, end):
+        if begin > end:
+            return None
+
+        middle = (begin + end) / 2
+        num = nums[middle]
+        node = TreeNode(num)
+        node.left = self.do(nums, begin, middle - 1)
+        node.right = self.do(nums, middle + 1, end)
+        return node
+
+if __name__ == "__main__":
+    solution = Solution()
+    head = ListNode.create([0])
+    head = solution.sortedListToBST(head)
+    print head

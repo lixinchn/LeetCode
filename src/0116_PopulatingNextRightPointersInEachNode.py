db1fd7023e2aa603db0370b60e95addb139fe970
lixinchn <lixinit@gmail.com>
2016-08-13 22:02:57 +0800

    populating next right pointers each node

---
+++ src/0116_PopulatingNextRightPointersInEachNode.py
@@ -0,0 +1,57 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.next = None
+
+    @staticmethod
+    def create(arr):
+        head = None
+        this = None
+        left, right = False, False
+        prev_nodes = []
+        for val in arr:
+            node = TreeNode(val)
+            if not head:
+                head = node
+                this = node
+                continue
+
+            if left and right:
+                this = prev_nodes.pop(0)
+                left = False
+                right = False
+            if not left:
+                this.left = node
+                left = True
+            elif not right:
+                this.right = node
+                right = True
+            if node and node.val:
+                prev_nodes.append(node)
+
+        return head
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: TreeLinkNode
+        :rtype: nothing
+        """
+        self.do(root, None)
+
+    def do(self, root, next):
+        if not root:
+            return
+        root.next = next
+        self.do(root.left, root.right)
+        self.do(root.right, root.next.left if root.next else None)
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    head = TreeNode.create([1,2,3,4,5,6,7])
+    solution.connect(head)
+    print head

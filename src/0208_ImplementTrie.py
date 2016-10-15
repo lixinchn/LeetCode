96bcd174caa34fb801247bd237f41d1337795d88
lixinchn <lixinit@gmail.com>
2016-10-16 00:48:56 +0800

    implement trie

---
+++ src/0208_ImplementTrie.py
@@ -0,0 +1,59 @@
+class TrieNode(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.children = dict()
+        self.finish = False
+        
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for letter in word:
+            child = node.children.get(letter)
+            if child is None:
+                child = TrieNode()
+                node.children[letter] = child
+            node = child
+        node.finish = True
+        
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for letter in word:
+            node = node.children.get(letter)
+            if node is None:
+                return False
+        return node.finish
+        
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie
+        that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for letter in prefix:
+            node = node.children.get(letter)
+            if node is None:
+                return False
+        return True
+        
+

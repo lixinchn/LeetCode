d363d05764ff11726a00d48a80809048ec66706d
lixinchn <lixinit@gmail.com>
2016-10-18 09:36:01 +0800

    course schedule II

---
+++ src/0210_CourseScheduleII.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        degrees = [0] * numCourses
+        childs = [[] for x in range(numCourses)]
+        for pair in prerequisites:
+            degrees[pair[0]] += 1
+            childs[pair[1]].append(pair[0])
+        courses = set(range(numCourses))
+        flag = True
+        ans = []
+        while flag and len(courses):
+            flag = False
+            removeList = []
+            for x in courses:
+                if degrees[x] == 0:
+                    for child in childs[x]:
+                        degrees[child] -= 1
+                    removeList.append(x)
+                    flag = True
+            for x in removeList:
+                ans.append(x)
+                courses.remove(x)
+        return [[], ans][len(courses) == 0]
+
+        
+if __name__ == "__main__":
+    solution = Solution()
+    print solution.findOrder(2, [[1,0], [0,1]])
